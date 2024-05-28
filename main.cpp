13772a8a58dffb33c8f5926d1b9cfc5f03224352
Jędrzej Stańczewski <jstanczewski99@gmail.com>
2024-05-28 23:10:30 +0200

    mainly graphic update
    graphic changes, tracks score and displays it and the game title, background is now chess-like, colors adjusted

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <SDL.h>
+#include <SDL_ttf.h>
 #include <iostream>
 #include <vector>
 #include <ctime>
+#include <string>
 
-// Screen dimension constants
+const int HEADER_HEIGHT = 100;
 const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 800;
-const int UNIT_SIZE = 20;
+const int SCREEN_HEIGHT = 800 + HEADER_HEIGHT;
+const int NUM_UNITS_X = 20;
+const int NUM_UNITS_Y = (SCREEN_HEIGHT - HEADER_HEIGHT) / ((SCREEN_WIDTH / NUM_UNITS_X));
+const int UNIT_SIZE = SCREEN_WIDTH / NUM_UNITS_X;
+const int FONT_SIZE = 32;
 
 enum Direction { UP, DOWN, LEFT, RIGHT };
 
@@ -17,7 +22,7 @@ struct Position {
 class Snake {
 public:
     Snake() {
-        segments.push_back({ 0, SCREEN_HEIGHT / 2 });
+        segments.push_back({ 0, HEADER_HEIGHT + (SCREEN_HEIGHT - HEADER_HEIGHT) / 2 });
         direction = RIGHT;
     }
 
@@ -54,7 +59,7 @@ public:
 
     bool checkCollision() {
         Position head = getHeadPosition();
-        if (head.x < 0 || head.x >= SCREEN_WIDTH || head.y < 0 || head.y >= SCREEN_HEIGHT) {
+        if (head.x < 0 || head.x >= SCREEN_WIDTH || head.y < HEADER_HEIGHT || head.y >= SCREEN_HEIGHT) {
             return true;
         }
 
@@ -67,10 +72,13 @@ public:
     }
 
     void render(SDL_Renderer* renderer) {
-        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-        for (const Position& pos : segments) {
-            SDL_Rect fillRect = { pos.x, pos.y, UNIT_SIZE, UNIT_SIZE };
-            SDL_RenderFillRect(renderer, &fillRect);
+        for (size_t i = 0; i < segments.size(); ++i) {
+            if (i == 0) {
+                SDL_SetRenderDrawColor(renderer, 0, 100, 0, 255);
+            } else {
+                SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+            }
+            drawRoundedRect(renderer, segments[i].x, segments[i].y, UNIT_SIZE, UNIT_SIZE, 5);
         }
     }
 
@@ -82,6 +90,26 @@ private:
     std::vector<Position> segments;
     Direction direction;
     bool growing = false;
+
+    void drawRoundedRect(SDL_Renderer* renderer, int x, int y, int w, int h, int r) {
+        SDL_Rect rects[3] = {
+            { x + r, y, w - 2 * r, h },
+            { x, y + r, r, h - 2 * r },
+            { x + w - r, y + r, r, h - 2 * r }
+        };
+        SDL_RenderFillRects(renderer, rects, 3);
+
+        for (int i = 0; i < r; ++i) {
+            for (int j = 0; j < r; ++j) {
+                if ((i * i + j * j) <= (r * r)) {
+                    SDL_RenderDrawPoint(renderer, x + r - i, y + r - j);
+                    SDL_RenderDrawPoint(renderer, x + w - r + i, y + r - j);
+                    SDL_RenderDrawPoint(renderer, x + r - i, y + h - r + j);
+                    SDL_RenderDrawPoint(renderer, x + w - r + i, y + h - r + j);
+                }
+            }
+        }
+    }
 };
 
 class Food {
@@ -92,7 +120,7 @@ public:
 
     void respawn() {
         position.x = (rand() % (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
-        position.y = (rand() % (SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
+        position.y = HEADER_HEIGHT + (rand() % ((SCREEN_HEIGHT - HEADER_HEIGHT) / UNIT_SIZE)) * UNIT_SIZE;
     }
 
     Position getPosition() const {
@@ -100,18 +128,71 @@ public:
     }
 
     void render(SDL_Renderer* renderer) {
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-        SDL_Rect fillRect = { position.x, position.y, UNIT_SIZE, UNIT_SIZE };
-        SDL_RenderFillRect(renderer, &fillRect);
+        drawCircle(renderer, position.x + UNIT_SIZE / 2, position.y + UNIT_SIZE / 2, UNIT_SIZE / 2);
     }
 
 private:
     Position position;
+
+    void drawCircle(SDL_Renderer* renderer, int centerX, int centerY, int radius) {
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        for (int w = 0; w < radius * 2; w++) {
+            for (int h = 0; h < radius * 2; h++) {
+                int dx = radius - w;
+                int dy = radius - h;
+                if ((dx * dx + dy * dy) <= (radius * radius)) {
+                    SDL_RenderDrawPoint(renderer, centerX + dx, centerY + dy);
+                }
+            }
+        }
+    }
 };
+
+void renderChessboard(SDL_Renderer* renderer) {
+    bool white = true;
+    for (int y = HEADER_HEIGHT; y < SCREEN_HEIGHT; y += UNIT_SIZE) {
+        for (int x = 0; x < SCREEN_WIDTH; x += UNIT_SIZE) {
+            if (white) {
+                SDL_SetRenderDrawColor(renderer, 210, 210, 210, 255);
+            } else {
+                SDL_SetRenderDrawColor(renderer, 180, 180, 180, 255);
+            }
+            SDL_Rect fillRect = { x, y, UNIT_SIZE, UNIT_SIZE };
+            SDL_RenderFillRect(renderer, &fillRect);
+            white = !white;
+        }
+        white = !white;
+    }
+}
+
+void renderHeader(SDL_Renderer* renderer, TTF_Font* font, int score) {
+    SDL_Color black = { 0, 0, 0, 255 };
+
+    SDL_Surface* surfaceMessage = TTF_RenderText_Solid(font, "Wonsz", black);
+    SDL_Texture* message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
+    SDL_Rect messageRect = { 40, 10, surfaceMessage->w, surfaceMessage->h };
+    SDL_RenderCopy(renderer, message, NULL, &messageRect);
+    SDL_FreeSurface(surfaceMessage);
+    SDL_DestroyTexture(message);
+
+    std::string scoreText = "Score: " + std::to_string(score);
+    surfaceMessage = TTF_RenderText_Solid(font, scoreText.c_str(), black);
+    message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
+    messageRect = { SCREEN_WIDTH / 2, 10, surfaceMessage->w, surfaceMessage->h };
+    SDL_RenderCopy(renderer, message, NULL, &messageRect);
+    SDL_FreeSurface(surfaceMessage);
+    SDL_DestroyTexture(message);
+}
 
 int main(int argc, char* args[]) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    if (TTF_Init() == -1) {
+        std::cerr << "TTF_Init: " << TTF_GetError() << std::endl;
+        SDL_Quit();
         return 1;
     }
 
@@ -130,12 +211,22 @@ int main(int argc, char* args[]) {
         return 1;
     }
 
+    TTF_Font* font = TTF_OpenFont("C:\\Users\\Jedrzej\\Desktop\\sgd_project\\assets\\arial.ttf", FONT_SIZE);
+    if (!font) {
+        std::cerr << "Failed to load font: " << TTF_GetError() << std::endl;
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
     srand(static_cast<unsigned>(time(0)));
 
     bool quit = false;
     SDL_Event e;
     Snake snake;
     Food food;
+    int score = 0;
 
     while (!quit) {
         while (SDL_PollEvent(&e) != 0) {
@@ -156,14 +247,20 @@ int main(int argc, char* args[]) {
         if (snake.getHeadPosition().x == food.getPosition().x && snake.getHeadPosition().y == food.getPosition().y) {
             snake.grow();
             food.respawn();
+            score += 10;
         }
 
         if (snake.checkCollision()) {
             quit = true;
         }
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 0, 100, 0, 255);
+        SDL_Rect headerRect = { 0, 0, SCREEN_WIDTH, HEADER_HEIGHT };
+        SDL_RenderFillRect(renderer, &headerRect);
+
+        renderHeader(renderer, font, score);
+
+        renderChessboard(renderer);
 
         snake.render(renderer);
         food.render(renderer);
@@ -173,6 +270,8 @@ int main(int argc, char* args[]) {
         SDL_Delay(150);
     }
 
+    TTF_CloseFont(font);
+    TTF_Quit();
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
